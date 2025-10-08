5171e6753cfe4b5a4fab0276c2ba172f26b7ec80
Boris Spremo <boris.spremo@rs.monochrome.co.uk>
2025-10-08 11:57:41 +0200

    tile-campaign completed

--- wp-content/themes/dlap/template-parts/tiles/tile-m2-campaign.php
+++ wp-content/themes/dlap/template-parts/tiles/tile-m2-campaign.php
@@ -1,9 +1,13 @@
 <?php // print_r($args); ?>
 
 
-<div class="box px-4 py-5 items-center">
-    <div class="tile-title"><?php echo $args['title'] ?? ''; ?></div>
-    <div class="tile-description"><?php echo $args['description'] ?? ''; ?></div>
-    <div class="tile-link"><?php echo $args['link'] ?? ''; ?></div>
-    <div class="tile-link"><?php echo $args['background'] ?? ''; ?></div>
+<div class="tile-block tile-campaign box px-4 py-5 items-center" style="background-image: url('<?php echo $args['backgroundUrl'] ?? ''; ?>');">
+    <div class="tile-content">
+        <h2 class="tile-title"><?php echo $args['title'] ?? ''; ?></h2>
+        <span class="tile-description"><?php echo $args['description'] ?? ''; ?></span>
+        <a href="<?php echo $args['link'] ?? ''; ?>" class="tile-link">
+            Read more
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/link-external.png" alt="read more" />
+        </a>
+    </div>
 </div>
