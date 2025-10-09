9a20b146a6a1f3e8262b1e04bad5404f142f636d
Boris Spremo <boris.spremo@rs.monochrome.co.uk>
2025-10-09 16:26:23 +0200

    fix for footer

--- wp-content/themes/dlap/footer.php
+++ wp-content/themes/dlap/footer.php
@@ -14,7 +14,7 @@
 <?php if ($footnote): ?>
     <div style="    font-style: italic;
     font-size: 0.75rem;
-    text-align: right;
+    text-align: left;
     opacity: 0.6;
     color: white;"><?php echo $footnote; ?></div>
 <?php endif; ?>
