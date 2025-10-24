7a94efa566b3c6e092658b4a5b6ab8197c59a8ba
Boris Spremo <boris.spremo@rs.monochrome.co.uk>
2025-10-24 13:44:18 +0200

    fix for the tile-6

--- wp-content/themes/dlap/template-parts/tiles/tile-6.php
+++ wp-content/themes/dlap/template-parts/tiles/tile-6.php
@@ -3,11 +3,11 @@ $display = 'flex items-start';
 $liStyle = '';
 if(!isset($args['image']) && (isset($args['title']) || isset($args['text'])) && isset($args['items'])) {
     $display = 'grid items-center';
-    $liStyle = 'margin-bottom: 10px;font-size:0.9rem"';
+    $liStyle = 'margin-bottom: 10px; font-size:12px"';
 }
 ?>
 
-<div class="theicon-with-text box !py-6 gap-6  <?php echo  isset($args['is_small']) && $args['is_small'] == true ? 'items-center' : 'items-start'; ?> justify-start <?php echo $display; ?>">
+<div class="theicon-with-text tile-background box !py-6 gap-6  <?php echo  isset($args['is_small']) && $args['is_small'] == true ? 'items-center' : 'items-start'; ?> justify-start <?php echo $display; ?>">
     <div class="tile-header">
         <?php if (isset($args['image'])): ?>
             <div class="tile-icon <?php echo isset($args['is_small']) && $args['is_small'] == true ? 'flex' : 'grid'; ?> items-start text-center pt-1">
@@ -30,8 +30,8 @@ if(!isset($args['image']) && (isset($args['title']) || isset($args['text'])) && 
         <?php endif; ?>
     </div>
     <?php if (isset($args['items']) && count($args['items']) && !empty($args['items'])): ?>
-        <div class="tile-container w-full pl-4 items-center text-left">
-            <ul class="text-3 list-disc pb-8px leading-normal">
+        <div class="tile-container w-full pl-2 items-center text-left">
+            <ul class="text-3 list-disc leading-normal">
                 <?php foreach ($args['items'] as $item): ?>
                     <?php
                         $styles = 'style="'.$liStyle.'"';
